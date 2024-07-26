e03c5c000c8cda26a14061b1981067ca91afd4a1
kinestrik <shadlovskiyy@bk.ru>
2024-07-26 23:28:32 +0300

    Распределение по папкам + роутинг

---
+++ client/src/componets/orders/MyordersPage.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+    
+}
+</script>
+<template>
+    d
+</template>
+<style scoped>
+
+</style>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,23 +1,53 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: () => import('../componets/Main/MainPage.vue')
+      path: "/",
+      name: "home",
+      component: () => import("../componets/Main/MainPage.vue"),
     },
     {
-      path: '/Product',
-      name: 'product',
-      component: () => import('../componets/Product/ProductPage.vue')
+      path: "/Product",
+      name: "product",
+      component: () => import("../componets/Product/ProductPage.vue"),
     },
     {
-      path: '/Products',
-      name: 'product',
-      component: () => import('../componets/Product/ProductsPage.vue')
+      path: "/Login",
+      name: "login",
+      component: () => import("../componets/Forms/EmailPage.vue"),
     },
-  ]
-})
+    {
+      path: "/Enter",
+      name: "enter",
+      component: () => import("../componets/Forms/EnterPage.vue"),
+    },
+    {
+      path: "/Category",
+      name: "category",
+      component: () => import("../componets/Product/ProductsPage.vue"),
+    },
+    {
+      path: "/Favorites",
+      name: "favorites",
+      component: () => import("../componets/favorites/Favorites.vue"),
+    },
+    {
+      path: "/myorders",
+      name: "myorders",
+      component: () => import("../componets/orders/MyordersPage.vue"),
+    },
+    {
+      path: "/myorders/arkhiv",
+      name: "arkhiv",
+      component: () => import("../componets/orders/ArkhivPage.vue"),
+    },
+    {
+      path: "/AddProduct",
+      name: "AddProduct",
+      component: () => import("../componets/admin/AddProduct.vue"),
+    },
+],
+});
 
-export default router
+export default router;
