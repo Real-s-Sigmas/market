8c675a24f76cdf758b160fe71e51c0366c2f992a
Maxim <justhabber@gmail.com>
2024-09-14 08:58:28 +0300

    О нас и карта

---
+++ client/src/componets/AboutUs/AboutComp.vue
@@ -0,0 +1,57 @@
+<script>
+import YandexMap from '../reuse/YandexMap.vue'
+
+export default {
+  components: {
+    YandexMap,
+  }
+}
+</script>
+
+<template>
+  <div class="wind">
+    <h1 class="about-title">О нас</h1>
+    <div class="img-text">
+      <img class="aboutus-img" src="../../assets/1111.jpeg" alt="">
+      <p class="text"> lalalalalalalalalalalalalal </p>
+    </div>
+    <h3>Как нас найти?</h3>
+    <div class="map">
+      <YandexMap :center="[ 55.784428, 37.236963 ]" zoom="15" /> 
+    </div>
+  </div>
+</template>
+
+<style>
+.wind {
+  display: flex;
+  flex-direction: column;
+  gap: 40px;
+  padding: 0 40px 0 40px;
+}
+
+.about-title {
+  font-size: 30px;
+  font-weight: 600;
+  border-bottom: 2px solid #000;
+  width: 90px;
+}
+
+.img-text {
+  display: flex;
+  gap: 60px;
+}
+
+.aboutus-img {
+  width: 450px;
+  border: 2px solid #000;
+  border-radius: 12px;
+}
+
+h3 {
+  font-size: 26px;
+  font-weight: 600;
+  border-bottom: 2px solid #000;
+  width: 200px;
+}
+</style>
---
+++ client/src/componets/reuse/YandexMap.vue
@@ -0,0 +1,44 @@
+<script>
+export default {
+    name: 'YandexMap',
+    props: {
+        center: {
+            type: Array,
+            default: () => [ 55.784428, 37.236963 ]
+        },
+        zoom: {
+            type: Number,
+            default: 10,
+        },
+    },
+
+    mounted() {
+        this.initMap();
+    },
+
+    methods: {
+        initMap() {
+            ymaps.ready(() => {
+                this.map = new ymaps.Map(this.$refs.yandexMap, {
+                    center: this.center,
+                    zoom: this.zoom,
+                });
+
+                const placemark = new ymaps.Placemark(this.center, {
+                    balloonContent: "Здесь ваша точка",
+                });
+
+                this.map.geoObjects.add(placemark);
+            })
+        }
+    }
+}
+</script>
+
+<template>
+   <div ref="yandexMap" style="width: 100%; height: 400px;"></div> 
+</template>
+
+<style>
+
+</style>
