35d6e8666dc0b7ead5279ce4e35db7574c07088b
Maxim <justhabber@gmail.com>
2024-12-28 18:21:45 +0300

    Из хедера убрано все ненужное

--- client/src/componets/reuse/HeaderPage.vue
+++ client/src/componets/reuse/HeaderPage.vue
@@ -101,23 +101,7 @@ export default {
           </svg>
         </a>
 
-        <button v-if="isLogged" @click="burgmenu" class="burgmenu">
-          <svg
-            width="35px"
-            height="35px"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M4 6H20M4 12H20M4 18H20"
-              stroke="#000000"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-          </svg>
-        </button>
+        
         <button
           v-if="!isLogged && !isAdmin"
           type="button"
@@ -217,23 +201,7 @@ export default {
         </svg>
       </a>
 
-      <button v-if="isLogged" @click="burgmenu" class="burgmenu">
-        <svg
-          width="35px"
-          height="35px"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M4 6H20M4 12H20M4 18H20"
-            stroke="#000000"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-        </svg>
-      </button>
+      
       <button
           v-if="!isLogged && !isAdmin"
           type="button"
