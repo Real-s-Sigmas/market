c81c773ab73d88143965c3b01013329fbeb20a4a
Konstantin <151402518+Willyamdevon@users.noreply.github.com>
2024-12-31 03:07:52 +0300

    Update photos.py

--- server/photos.py
+++ server/photos.py
@@ -1,4 +1,4 @@
-import os, base64, logging, re
+import os, base64, logging, re, base64
 
 from app import app, PASSWORD_PG, PORT_PG, USER_PG, HOST_PG, MEDIA, AVATAR
 from flask import Flask, jsonify, request, session, make_response, send_from_directory
@@ -15,26 +15,24 @@ load_dotenv()
 PATTERN = os.getenv('PATTERN')
 API_URL = os.getenv('API_URL')
 
-
 logging.info("photos.py have connected")
 
 
 def AddPhoto(base: str, type: str, id: str) -> str:
+    mime_type = "."+base[len("data:image/"):base.find(";base64")]
     base=base[base.find(',')+1:]
     decoded_bytes = base64.b64decode(base)
-    dote = name[name.find('.'):]
-    name = type+'_'+id+dote
+    name = type+'_'+id+mime_type
     with open(os.path.join(MEDIA, name), "wb") as file:
         file.write(decoded_bytes)
     return API_URL+name
 
 def DeletePhotoS(links: list) -> str:
     for link in links:
-        match = re.search(PATTERN, link)
-        if match:
-            name = match.group(1)
-            if os.path.exists("/items/"+name):
-                os.remove("/items/"+name)
+        name = link[link.find(API_URL)+len(API_URL):]
+        if os.path.exists(os.path.join(MEDIA, name)):
+            logging.info(name)
+            os.remove(os.path.join(MEDIA, name))
 
 @app.route('/items/<path:filename>')
 def serve_file(filename):
