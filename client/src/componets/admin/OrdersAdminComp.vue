02bca6961b1d336949eb4fbf375c3880a6d3d95d
Garleenn <garleenn@gmail.com>
2025-01-02 18:32:11 +0300

    Цвета у заказов

--- client/src/componets/admin/OrdersAdminComp.vue
+++ client/src/componets/admin/OrdersAdminComp.vue
@@ -14,9 +14,10 @@
 
 <template>
   <a :href="`/basketadmin/${order.id}`">
-    <div class="comp-block">
+    <div class="comp-block" :class="{'grey': order.status == 'END', 'yellow': order.status == 'WAITING', 'green': order.status == 'PROCESS'}">
       <p class='font-bold'>Номер пользователя: {{ order.phonenumber }}</p>
       <p>Количество товаров: {{ order.ids_items.length }}</p>
+      <p>Статус: {{ order.status }}</p>
       <p>{{ order.date_create }}</p>
     </div>
   </a>
@@ -24,6 +25,18 @@
 </template>
 
 <style scoped>
+.grey {
+  background: rgb(201, 199, 199);
+  border-color: rgb(201, 199, 199) !important;
+}
+
+.yellow {
+  border-color: rgb(157, 255, 0) !important;
+}
+
+.green {
+  border-color: rgb(255, 238, 0) !important;
+}
 
 a {
   width: 300px;
